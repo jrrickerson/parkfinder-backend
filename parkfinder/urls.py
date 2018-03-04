424cdea7134ea1da2a59f032ae0fcb3eb10cb0c4
JR Rickerson <jrrickerson@gmail.com>
2018-03-03 21:53:41 -0500

    Add placeholder home view.

--- parkfinder/urls.py
+++ parkfinder/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
+    path('', include('parkfinder.parks.urls')),
 ]
